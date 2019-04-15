127bf70bd2e0f4918aeb87b00b788fa7927dd6ba
Ayush Ranjan <ayushr2@illinois.edu>
2019-04-15 02:19:54 -0500

    enough for today

--- pocketDoc/DetailViewController.swift
+++ pocketDoc/DetailViewController.swift
@@ -24,29 +24,68 @@ import UIKit
 
 class DetailViewController: UIViewController {
   
-  @IBOutlet weak var detailDescriptionLabel: UILabel!
-  @IBOutlet weak var candyImageView: UIImageView!
-  @IBOutlet weak var detailDescription: UITextView!
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+    @IBOutlet weak var candyImageView: UIImageView!
+    @IBOutlet weak var detailDescription: UITextView!
+    @IBOutlet weak var actionBtn: UIButton!
+    @IBAction func doAction(_ sender: Any) {
+        switch detailCandy!.category {
+        case Category.Hospital:
+            dialNumber(number: "2173051108")
+        case Category.Pharmacy:
+            dialNumber(number: "2173051108")
+        default:
+            print("lol")
+        }
+    }
     
-  var detailCandy: SearchOption? {
-    didSet {
-      configureView()
+    var detailCandy: SearchOption? {
+        didSet {
+            configureView()
+        }
     }
-  }
   
-  func configureView() {
-    if let detailCandy = detailCandy {
-      if let detailDescriptionLabel = detailDescriptionLabel, let candyImageView = candyImageView, let detailDescription = detailDescription {
-        detailDescriptionLabel.font = UIFont(name: "Verdana-Bold", size: 20)
-        detailDescriptionLabel.text = detailCandy.name
-        candyImageView.image = UIImage(named: detailCandy.name)
-        title = detailCandy.category.toString()
-        detailDescription.isScrollEnabled = false
-        detailDescription.font = UIFont(name: "Verdana", size: 16)
-        detailDescription.text = detailCandy.description
-      }
+    func configureView() {
+        if let detailCandy = detailCandy {
+          if let detailDescriptionLabel = detailDescriptionLabel, let candyImageView = candyImageView, let detailDescription = detailDescription, let actionBtn = actionBtn {
+            detailDescriptionLabel.font = UIFont(name: "Verdana-Bold", size: 20)
+            detailDescriptionLabel.text = detailCandy.name
+            
+            candyImageView.image = UIImage(named: detailCandy.name)
+            
+            title = detailCandy.category.toString()
+            
+            detailDescription.isScrollEnabled = false
+            detailDescription.font = UIFont(name: "Verdana", size: 16)
+            detailDescription.text = detailCandy.description
+            
+            switch detailCandy.category {
+                case Category.Hospital:
+                    actionBtn.setTitle("Call", for: .normal)
+                case Category.Pharmacy:
+                    actionBtn.setTitle("Call", for: .normal)
+                case Category.Medicine:
+                    actionBtn.setTitle("Buy", for: .normal)
+                case Category.Symptom:
+                    actionBtn.setTitle("Remedies", for: .normal)
+            }
+          }
+        }
     }
-  }
+    
+    func dialNumber(number : String) {
+        
+        if let url = URL(string: "tel://\(number)"),
+            UIApplication.shared.canOpenURL(url) {
+            if #available(iOS 10, *) {
+                UIApplication.shared.open(url, options: [:], completionHandler:nil)
+            } else {
+                UIApplication.shared.openURL(url)
+            }
+        } else {
+            // add error message here
+        }
+    }
   
   override func viewDidLoad() {
     super.viewDidLoad()
